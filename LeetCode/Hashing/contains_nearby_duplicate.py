9f95a04f9a7f590da44ceb14a192ebff31df8994
yiseboge <Ethiopia1!>
2020-01-15 18:05:34 +0300

    add contains duplicate

---
+++ LeetCode/Hashing/contains_nearby_duplicate.py
@@ -0,0 +1,37 @@
+import sys
+
+
+class Solution:
+    def containsNearbyDuplicate(self, nums, k: int) -> bool:
+        if k == 0:
+            return False
+        buffer = set()
+        for i in range(len(nums)):
+            x = nums[i]
+            if x in buffer:
+                return True
+            if len(buffer) < k:
+                buffer.add(x)
+            else:
+                buffer.remove(nums[i - k])
+                buffer.add(x)
+
+        return False
+
+
+def solution(l1, l2):
+    s = Solution()
+    return s.containsNearbyDuplicate(l1, l2)
+
+
+def main():
+    # inp1 = sys.stdin.readline().split()
+    # inp2 = sys.stdin.readline().split()
+
+    inp1 = [1, 2, 3, 1]
+    inp2 = 3
+    sys.stdout.write(str(solution(inp1, inp2)))
+
+
+if __name__ == '__main__':
+    main()
